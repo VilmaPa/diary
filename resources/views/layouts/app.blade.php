6bf873b2ea79a71949cd5263bf0180fb83d58f74
VilmaPa <70938111+VilmaPa@users.noreply.github.com>
2021-02-14 14:57:58 +0200

    small fixes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,7 +27,7 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
+                <a style="font-family:'Papyrus', 'Lucida Handwriting', monospace; font-weight: bold;" class="navbar-brand" href="{{ url('/students') }}">
                     Dienynas
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
